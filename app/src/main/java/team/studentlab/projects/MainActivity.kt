9315fb3a0b7ebc3fb0deb12b0ecffaeeaf3328fa
Carlos Eduardo <ce64198@gmail.com>
2024-09-10 22:59:53 -0300

    Turn to scaffold method in this class

--- app/src/main/java/team/studentlab/projects/MainActivity.kt
+++ app/src/main/java/team/studentlab/projects/MainActivity.kt
@@ -1,20 +1,27 @@
 package team.studentlab.projects
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import team.studentlab.projects.ui.screens.Init
+import androidx.compose.material3.Scaffold
+import team.studentlab.projects.ui.parts.RecentPost
+import team.studentlab.projects.ui.parts.TopBar
 import team.studentlab.projects.ui.theme.TechCommunityTheme
 
 class MainActivity : ComponentActivity() {
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TechCommunityTheme {
-                Init();
+                Scaffold(
+                    topBar = { TopBar() },
+                    content = { RecentPost() }
+                )
             }
         }
     }
